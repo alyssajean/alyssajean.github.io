9dfd0338bb436d89bb4c6734592b165add259df6
Alyssa <akabigting@seakeeper.com>
2024-06-26 16:36:43 -0700

    cname

--- js/scripts.js
+++ js/scripts.js
@@ -163,6 +163,13 @@ $(function () {
     $('.player').YTPlayer();
 
 
+    document.addEventListener('touchstart', function(event) {
+        // Check if the event is on a link
+        if(event.target.tagName === 'A') {
+          event.target.click();
+        }
+      }, false);
+      
     /********************** Toggle Map Content **********************/
     $('#btn-show-map').click(function () {
         $('#map-content').toggleClass('toggle-map-content');
